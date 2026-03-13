eb4c5b7368948dd082f1d07430c18bfc8dd76cf7
OrnellaMarion <168301329+NellaOne@users.noreply.github.com>
2026-03-13 14:55:03 +0300

    sprint5/1/database_config

---
+++ base/data/sprint5/init_staging.sql
@@ -0,0 +1,58 @@
+-- 2026-03-13 : Initialisation des donnees de test pour sprint5 (staging)
+
+INSERT INTO staging.Hotel (code, nom) VALUES
+('AER', 'Aeroport'),
+('PAN', 'Panache'),
+('PCH', 'Pachniko'),
+('CLB', 'Colbert'),
+('NOV', 'Novotel'),
+('IBS', 'Ibis'),
+('HBS', 'Hibiscus');
+
+INSERT INTO staging.Vehicule (Reference, nbPlace, TypeVehicule) VALUES
+('VH-D-007', 7, 'D'),
+('VH-D-011', 11, 'D'),
+('VH-E-012', 12, 'E'),
+('VH-E-018', 18, 'E');
+
+INSERT INTO staging.token_expiration (token, expiration) VALUES
+('550e8400-e29b-41d4-a716-446655440001', NOW() + INTERVAL '24 hours'),
+('6ba7b810-9dad-11d1-80b4-00c04fd430c9', NOW() + INTERVAL '48 hours');
+
+INSERT INTO staging.Parametre (code, valeur, unite, typeValeur) VALUES
+('Vm', '30', 'km', 'Integer'),
+('temps_attente', '30', 'minutes', 'Integer');
+
+-- Distances (airport -> hotels and between hotels)
+INSERT INTO staging.Distance (from_hotel, to_hotel, km) VALUES
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), 12),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), 18),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), 25),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), 15),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 21),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'AER'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), 15),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), 5),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), 20),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), 3),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 14),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), 10),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), 8),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), 11),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), 16),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 5),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), 7),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), 9),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 6),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), 4),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 12),
+((SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PCH'), (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'IBS'), 8);
+
+-- Reservations de test Sprint5 (2026-03-20)
+-- Fenetre groupe: 04:20 + 30 min = 04:50
+-- Groupe attendu: [04:20, 04:45, 04:45, 04:47]
+-- heureDepartGroupe attendue: 04:47 (max)
+INSERT INTO staging.reservation (DateHeureArrive, idClient, nbPassager, Id_Hotel) VALUES
+('2026-03-20 04:20:00', 'CLI-S5-001', 7, (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'CLB')),
+('2026-03-20 04:45:00', 'CLI-S5-002', 4, (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'NOV')),
+('2026-03-20 04:45:00', 'CLI-S5-003', 2, (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'HBS')),
+('2026-03-20 04:47:00', 'CLI-S5-004', 1, (SELECT Id_Hotel FROM staging.Hotel WHERE code = 'PAN'));
